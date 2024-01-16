a80409daf4d4639a44be31bb8f3f2ce1f7ccdcad
Atharva Nagore <atharvanagore2004@gmail.com>
2024-01-16 16:49:06 +0530

    created login controleer

--- server/controllers/auth/postLogin.js
+++ server/controllers/auth/postLogin.js
@@ -1,3 +1,25 @@
-export const postLogin = async (req,res) => {
-    res.send('Login')
-}
+import User from "../../models/user.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const postLogin = async (req, res) => {
+  try {
+    const { mail, password } = req.body;
+
+    const user = await User.findOne({ mail: mail.toLowerCase() });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = "JWT_TOKEN";
+      return res.status(200).json({
+        userDetails: {
+          mail: user.mail,
+          token: token,
+          username: user.username,
+        },
+      });
+    }
+    return res.status(400).send("Invalid Credentials! Please try again");
+  } catch (err) {
+    return res.status(500).send("Something went wrong! Please try again");
+  }
+};
