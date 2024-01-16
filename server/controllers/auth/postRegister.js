435e15db4742e230789d433cf4bfcd0df97c5318
Atharva Nagore <atharvanagore2004@gmail.com>
2024-01-16 16:40:52 +0530

    created register controller

--- server/controllers/auth/postRegister.js
+++ server/controllers/auth/postRegister.js
@@ -1,3 +1,32 @@
-export const postRegister = async (req,res) => {
-    res.send('Register')
-}
+import User from "../../models/user.js";
+import bcrypt from "bcryptjs";
+
+export const postRegister = async (req, res) => {
+  try {
+    const { username, mail, password } = req.body;
+    const userExists = await User.exists({ mail: mail.toLowerCase() });
+    if (userExists) {
+      return res.status(409).send("E-mail already in use.");
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      mail: mail.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    const token = "JWT TOKEN";
+
+    res.status(201).json({
+      userDetails: {
+        mail: user.mail,
+        token: token,
+        username: user.username,
+      },
+    });
+  } catch (err) {
+    return res.status(500).send("Error occured! Please try again");
+  }
+};
