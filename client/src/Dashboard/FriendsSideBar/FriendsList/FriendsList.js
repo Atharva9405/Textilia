6a3af83d9b1b99987a8894b33f57c21f581aa4c0
Atharva Nagore <atharvanagore2004@gmail.com>
2024-01-18 22:24:51 +0530

    completed friends list ui

--- client/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.js
+++ client/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.js
@@ -1,33 +1,43 @@
-import React from 'react'
-import {styled} from '@mui/system'
+import React from "react";
+import { styled } from "@mui/system";
+import FriendsListItem from "./FriendsListItem";
 
 const DUMMY_FRIENDS = [
-    {
-      id: 1,
-      username: "Mark",
-      isOnline: true,
-    },
-    {
-      id: 2,
-      username: "Anna",
-      isOnline: false,
-    },
-    {
-      id: 3,
-      username: "John",
-      isOnline: false,
-    },
-  ];
+  {
+    id: 1,
+    username: "Mark",
+    isOnline: true,
+  },
+  {
+    id: 2,
+    username: "Anna",
+    isOnline: false,
+  },
+  {
+    id: 3,
+    username: "John",
+    isOnline: false,
+  },
+];
 
 const MainContainer = styled("div")({
-    flexGrow: 1,
-    width: "100%",
-  });
+  flexGrow: 1,
+  width: "100%",
+});
 
 const FriendsList = () => {
   return (
-    <MainContainer></MainContainer>
-  )
-}
+    <MainContainer>
+      {DUMMY_FRIENDS.map((f) => (
+        <FriendsListItem
+          username={f.username}
+          id={f.id}
+          key={f.id}
+          isOnline={f.isOnline}
+        />
+      ))}
+    </MainContainer>
+  );
+};
 
-export default FriendsList
+export default FriendsList;
