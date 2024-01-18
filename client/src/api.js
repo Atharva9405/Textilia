ad4cf4fe3f4340b381bd2b49dad4fcc4fb1dff17
Atharva Nagore <atharvanagore2004@gmail.com>
2024-01-18 12:32:28 +0530

    added jwt token in client side requests

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,20 @@
 import axios from "axios";
+import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:5000/api",
-  timeout: 10000000,
+  timeout: 10000,
+});
+
+apiClient.interceptors.request.use((config) => {
+  const userDetails = localStorage.getItem("user");
+  if (userDetails) {
+    const token = JSON.parse(userDetails).token;
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+},(err) => {
+  return Promise.reject(err)
 });
 
 export const login = async (data) => {
@@ -24,5 +36,12 @@ export const register = async (data) => {
       error: true,
       exception,
     };
+  }
+};
+
+const checkResponseCode = (exception) => {
+  const responseCode = exception?.response?.status;
+  if (responseCode) {
+    (responseCode === 401 || responseCode === 403) && logout();
   }
 };
