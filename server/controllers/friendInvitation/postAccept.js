fab8a9d679211dea43d73fd1095fee87c4801d7c
Atharva Nagore <atharvanagore2004@gmail.com>
2024-01-21 12:06:20 +0530

    implemented accept friend invitation logic

--- server/controllers/friendInvitation/postAccept.js
+++ server/controllers/friendInvitation/postAccept.js
@@ -1,5 +1,33 @@
-const postAccept = async (req,res) => {
-    return res.send("accept handler")
-}
+import FriendInvitation from "../../models/friendInvitation.js";
+import User from "../../models/user.js";
+import updateFriendsPendingInvitations from "../../socketHandlers/updates/friends.js";
+
+const postAccept = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const invitation = await FriendInvitation.findById(id);
+    if (!invitation) {
+      return res.status(401).send("Error occured! Please try again");
+    }
+
+    const { senderId, receiverId } = invitation;
+
+    const senderUser = await User.findById(senderId);
+    senderUser.friends = [...senderUser.friends, receiverId];
+
+    const receiverUser = await User.findById(receiverId);
+    receiverUser.friends = [...receiverUser.friends, senderId];
+    await senderUser.save();
+    await receiverUser.save();
+    await FriendInvitation.findByIdAndDelete(id);
+
+    updateFriendsPendingInvitations(receiverId.toString());
+    return res.status(200).send("Friend successfully added!");
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Something went wrong. Plase try again!");
+  }
+};
 
 export default postAccept;
+    
