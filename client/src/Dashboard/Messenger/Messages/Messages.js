8a1b45dc57bd0010cfc2ac6299fc22104566354e
Atharva Nagore <atharvanagore2004@gmail.com>
2024-02-02 23:03:46 +0530

    created date separator component

--- client/src/Dashboard/Messenger/Messages/Messages.js
+++ client/src/Dashboard/Messenger/Messages/Messages.js
@@ -3,6 +3,7 @@ import { styled } from "@mui/system";
 import MessagesHeader from "./MessagesHeader";
 import { connect } from "react-redux";
 import Message from "./Message";
+import DateSeparator from "./DateSeparator";
 
 const MainContainer = styled("div")({
   height: "calc(100% - 60px)",
@@ -39,18 +40,28 @@ const Messages = ({ chosenChatDetails, messages }) => {
               new Date(messages[index - 1]?.date),
               "dd/mm/yy"
             );
-        
-        console.log(messages)
 
         return (
-          <Message
-            key={message._id}
-            content={message.content}
-            username={message?.author?.username}
-            sameAuthor={sameAuthor}
-            date={convertDateToHumanReadable(new Date(message.date),"dd/mm/yy")}
-            sameDay={sameDay}
-          />
+          <div key={message._id} style={{ width: "97%" }}>
+            {(!sameDay || index === 0) && (
+              <DateSeparator
+                date={convertDateToHumanReadable(
+                  new Date(message.date),
+                  "dd/mm/yy"
+                )}
+              />
+            )}
+            <Message
+              content={message.content}
+              username={message?.author?.username}
+              sameAuthor={sameAuthor}
+              date={convertDateToHumanReadable(
+                new Date(message.date),
+                "dd/mm/yy"
+              )}
+              sameDay={sameDay}
+            />
+          </div>
         );
       })}
     </MainContainer>
